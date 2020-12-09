46dee68b5318a8a4f438928e70527a085a35e7b4
123kirito <2512064431@qq.com>
2020-12-09 22:17:07 +0800

    danche

--- AI/ai/car-detect/index.js
+++ AI/ai/car-detect/index.js
@@ -1,7 +1,24 @@
 //console.log('AI 来了，帅！')
 //node 引入模 块的关键字
 //图像识别
-const AipOcrClient=require('baidu-aip-sdk').ocr;
+const ApiImageClassifyClient = require('baidu-aip-sdk').imageClassify;
+//内置模块 fs模块 fileSystem
+const  fs=require('fs');
+ //1.读入文件 I/O操作 文件从硬盘中读入内存中
+const APP_ID="23077969"
+const API_KEY="8cfzO8otIk7NeX8q4Qvl5rpr"
+const SECRET_KEY="W8K0EYCSFdUbetUxwWSzwEVcGkZLBZYk"
+const client = new ApiImageClassifyClient(APP_ID,API_KEY,SECRET_KEY);
+const image=fs.readFileSync("野马.jpg").toString("base64");
+client
+    .carDetect(image)
+    .then(function(result){
+        console.log(result);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+    /*const AipOcrClient=require('baidu-aip-sdk').ocr;
 //内置模块 fileSystem
 const fs=require('fs');
 const image=fs.readFileSync("car.jpg").toString("base64");
@@ -21,16 +38,4 @@ client
     .catch(function(err){
         console.log(err);
     })
-
-/*const client=new ApiImageClassifyClient(APP_ID,API_KEY,SECRET_KEY);
-//const fs=require('fs');
-const image=fs.readFileSync("野马.jpg").toString("base64");
-
-client
-    .carDetect(image)
-    .then(function(result){
-        console.log(result);
-    })
-    .catch(function(err){
-        console.log(err);
-    })*/
+    */
