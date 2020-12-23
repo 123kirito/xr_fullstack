dbb55ad9db5a1f2b57feb355b2cdc70300aaddb2
123kirito <2512064431@qq.com>
2020-12-23 21:21:17 +0800

    book-phone

--- wxapp/book/miniprogram/pages/index/index.js
+++ wxapp/book/miniprogram/pages/index/index.js
@@ -1,27 +1,53 @@
-//不用买服务器 不用配置数据库
-const db=wx.cloud.database();//
-const phoneTable=db.collection("products");//
+//云数据库
+const db=wx.cloud.database()
+//userInfo表
+const userInfo=db.collection('userInfo')
 Page({
   data:{
-    phone_list:[]
+    userList:[]
   },
-  onLoad(){
-    //添加数据
-    db
-    .collection("products")
-    .get({
-      success:res=>{
-       // console.log(res);
-       this.setData({
-         phone_list:res.data
-       })
+    navigatTo(){
+
+    },
+    getUserInfo(result){
+     //console.log(result.detail.userInfo);
+     //一切的云开发未来都是从调用一个函数开始
+     //登录
+     wx.cloud.callFunction({
+      name: 'getOpenId',
+      //回调函数
+      complete: res => {
+        //继续怎么做
+        console.log(res)
+        //在哪里 mysql where 条件查询
+        userInfo.where({
+          // 去云端就是为了拿openId
+          // 用户的openId是唯一的，
+          _openid:res.result.openId
+        }).count().then( res => {
+          //console.log(res);
+          //新用户
+          if(res.total==0){
+            //插入用户
+            //Insert 插入
+            userInfo.add({
+              //一定要有一个data 一个JSON
+              data: result.detail.userInfo
+            }).then(res => {
+              wx.navigateTo({
+                url: '../add/add',
+              })
+            }).catch(err => {
+              console.error(err)
+            })
+           
+          } else {
+            wx.navigateTo({
+              url: '../add/add',
+            })
+          }
+        })
       }
     })
-  },
-  viewItem(e){
-    var id=e.currentTarget.dataSet.id;
-    wx.navigateTo({
-      url:'../phonedetail/phoneDetail?id='+id
-    })
-  }
+    }
 })
---
+++ wxapp/book/miniprogram/pages/index/readme.md
@@ -0,0 +1,6 @@
+- block 承载指令
+- wx:for wx:key 优化
+- 尊重用户的隐私
+  用户的头像、昵称。。。得到用户的认可
+  open-type="getUserInfo"
+  等用户来决定
