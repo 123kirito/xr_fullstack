26aed035753921f5b0345b474a8a030c86478a5d
123Kirito <2512064431@qq.com>
2021-03-30 22:20:15 +0800

    umi

---
+++ react/wumi/src/pages/about.tsx
@@ -0,0 +1,11 @@
+import './about.less';
+import {Link} from 'umi';
+
+export default function AboutPage(){
+    return(
+        <div>
+            <Link to="/">Index</Link>
+            Page About
+        </div>
+    )
+}
