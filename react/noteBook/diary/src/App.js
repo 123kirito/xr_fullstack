bdb5c316508e47f3e4ed7a282425c4d0fbbeab43
123Kirito <2512064431@qq.com>
2021-04-14 16:34:38 +0800

    first commit

---
+++ react/noteBook/diary/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from 'antd-mobile';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">测试</Button>
+    </div>
+  );
+}
+
+export default App;
